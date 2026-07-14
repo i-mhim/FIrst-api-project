22c09af69aaf4b1144d43ab80492a2d892a632d1
Sushant <sushantd.raut123@gmail.com>
2026-07-14 13:28:15 -0500

    adding firstgithub actions

--- app/main.py
+++ app/main.py
@@ -26,4 +26,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to my api!!!!"}
+    return {"message": "Hello World"}
---
+++ tests/test_posts.py
@@ -0,0 +1,110 @@
+from datetime import date
+import pytest
+from app import schema
+
+def test_get_all_posts(authorized_client, test_posts):
+    res = authorized_client.get("/posts/")
+    def validate(post):
+        return schema.PostOut(**post)
+    posts_map = map(validate, res.json())
+    posts_list = list(posts_map)
+    assert len(res.json()) == len(test_posts)
+    assert res.status_code == 200
+
+def test_unauthorized_user_get_all_posts(client, test_posts):
+    res = client.get("/posts/")
+    assert res.status_code == 401
+
+def test_unauthorized_user_get_one_post(client, test_posts):
+    res = client.get(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+def test_get_one_post_not_exit(authorized_client, test_posts):
+    res = authorized_client.get(f"/posts/8848")
+    assert res.status_code == 404
+
+def test_get_one_post(authorized_client, test_posts):
+    res = authorized_client.get(f"/posts/{test_posts[0].id}")
+    post = schema.PostOut(**res.json())
+    assert post.Post.id == test_posts[0].id
+    post.Post.content == test_posts[0].content
+    post.Post.title == test_posts[0].title
+
+@pytest.mark.parametrize("title, content, published", [
+    ("awesome new title", "awesome new contnent", True),
+    ("favorite pizza", "honululu hawaii", False),
+    ("tallest tree", "mantree", True)
+    ])
+def test_create_post(authorized_client, test_user, test_posts, title, content, published):
+    res = authorized_client.post("/posts/", json={"title": title, "content": content, "published": published})
+
+    created_post = schema.Post(**res.json())
+    assert res.status_code == 201
+    assert created_post.title == title
+    assert created_post.content == content
+    assert created_post.published == published
+    assert created_post.owner_id == test_user['id']
+
+def test_create_post_default_published_true(authorized_client, test_posts, test_user):
+    res = authorized_client.post("/posts/", json={"title": "arbitrary title", "content": "adfad"})
+
+    created_post = schema.Post(**res.json())
+    assert res.status_code == 201
+    assert created_post.title == "arbitrary title"
+    assert created_post.content == "adfad"
+    assert created_post.published == True
+    assert created_post.owner_id == test_user['id']
+
+def test_unauthorized_user_create_posts(client, test_posts, test_user):
+    res = client.post("/posts/", json={"title": "arbitrary title", "content": "adfad"})
+    assert res.status_code == 401
+
+def test_unauthorized_user_delete_Post(client, test_posts, test_user):
+    res = client.delete(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+def test_delete_post_success(authorized_client, test_posts, test_user):
+    res = authorized_client.delete(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 204
+
+def test_delete_post_non_exist(authorized_client, test_posts, test_user):
+    res = authorized_client.delete(f"/posts/9843297485372")
+    assert res.status_code == 404
+
+def test_delete_other_user_post(authorized_client, test_posts, test_user):
+    res = authorized_client.delete(f"/posts/{test_posts[3].id}")
+    assert res.status_code == 403
+
+def test_update_post(authorized_client, test_user, test_posts):
+    data = {
+        "title": "updated title", 
+        "content": "updated content",
+        "id": test_posts[0].id
+    }
+    res = authorized_client.put(f"/posts/{test_posts[0].id}", json=data)
+    updated_post = schema.Post(**res.json())
+    assert res.status_code == 200
+    assert updated_post.title == data['title']
+    assert updated_post.content == data['content']
+
+def test_update_other_user_post(authorized_client, test_user, test_posts, test_user2):
+    data = {
+        "title": "updated title", 
+        "content": "updated content",
+        "id": test_posts[3].id
+    }
+    res = authorized_client.put(f"/posts/{test_posts[3].id}", json=data)
+    assert res.status_code == 403
+
+def test_unauthorized_update_Post(client, test_posts, test_user):
+    res = client.put(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+def test_update_post_non_exist(authorized_client, test_posts, test_user):
+    data = {
+        "title": "updated title", 
+        "content": "updated content",
+        "id": test_posts[0].id
+    }
+    res = authorized_client.put(f"/posts/9843297485372", json=data)
+    assert res.status_code == 404
